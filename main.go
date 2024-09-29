ec9664ac5ad1584213195cb79cdcb439cb6300a5
Prakhar <pawasthi961@gmail.com>
2024-09-29 18:16:48 +0530

    credentials added to project

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
 func main() {
+	// Load the .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
 
 }
