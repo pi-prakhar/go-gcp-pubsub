3708165d0dcc3c940443a8abbe64d6411699e926
Prakhar <pawasthi961@gmail.com>
2024-09-29 18:17:19 +0530

    added publisher code

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 
+	"cloud.google.com/go/pubsub"
 	"github.com/joho/godotenv"
 )
 
@@ -12,5 +15,28 @@ func main() {
 	if err != nil {
 		log.Fatalf("Error loading .env file: %v", err)
 	}
+	ctx := context.Background()
+
+	// Create a client
+	client, err := pubsub.NewClient(ctx, PROJECT_ID)
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	}
+	defer client.Close()
+
+	// Create a topic (or use an existing one)
+	topic := client.Topic(TOPIC_ID)
+
+	// Publish a message
+	result := topic.Publish(ctx, &pubsub.Message{
+		Data: []byte("Hello, World!"),
+	})
+
+	// Get the result of the publish call
+	id, err := result.Get(ctx)
+	if err != nil {
+		log.Fatalf("Failed to publish message: %v", err)
+	}
+	fmt.Printf("Published message with ID: %s\n", id)
 
 }
