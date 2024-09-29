7d518e20d6d4e08ea0e274db0c32c450d3c46027
Prakhar <pawasthi961@gmail.com>
2024-09-29 18:15:25 +0530

    basic project

---
+++ constants.go
@@ -0,0 +1,8 @@
+package main
+
+const (
+	PROJECT_ID        = "poetic-orb-430304-r9"
+	TOPIC_ID          = "test-topic"
+	SUBSCRIPTION_ID_1 = "test-topic-sub-1"
+	SUBSCRIPTION_ID_2 = "test-topic-sub-2"
+)
